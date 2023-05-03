c5bfa986cbe9802b8469650a10d556fbb5b7f5da
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-05-03 19:32:22 -0400

    [core] add integrated payloadcms

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+// next.config.js
+const path = require('path');
+const { withPayload } = require('@payloadcms/next-payload');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const payloadConfig = {
+  // Point to your Payload config (Required)
+  configPath: path.resolve(__dirname, './payload/payload.config.ts'),
+  // Point to custom Payload CSS (optional)
+  // cssPath: path.resolve(__dirname, './css/my-custom-payload-styles.css'),
+  // Point to your exported, initialized Payload instance (optional, default shown below`)
+  payloadPath: path.resolve(process.cwd(), './payload/payloadClient.ts'),
+};
+
+module.exports = withPayload(nextConfig, payloadConfig);
