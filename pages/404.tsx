c3b19be8dffc17c6dad3128baae27d0cd8a8e328
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-05-15 03:52:59 -0400

    [site] add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,58 @@
+import Head from 'next/head';
+
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import AppFooter from '../components/AppFooter';
+import AppHeader from '../components/AppHeader';
+import { getPayloadClient } from '../payload/payloadClient';
+import { SECONDS_PER_DAY } from '../utils/constants';
+import { generateMetadataTags } from '../utils/opengraph-tags';
+import { FooterData, OpenGraphTags } from '../utils/types';
+
+type Props = {
+  metadata: OpenGraphTags;
+  footer: FooterData;
+}
+
+export default function NotFound({ metadata, footer }: Props) {
+  return (
+    <>
+      <Head>
+        {generateMetadataTags(metadata)}
+        <meta name="robots" content="noindex" />
+      </Head>
+      <AppHeader />
+      <main>
+        <Container maxWidth="lg">
+          <Typography variant='h1' gutterBottom sx={{ mt: '1rem' }}>Content not found</Typography>
+          <Typography variant='body1'>
+            Unfortunately, the content you are looking for is not available at this URL. Please try visiting the <Link href='/blog'>Blog Posts</Link> page
+            to see if the content you&apos;re looking for is available at a different URL.
+          </Typography>
+        </Container>
+      </main>
+      <AppFooter icons={footer.socialLinks} text={footer.copyrightText}/>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const payload = await getPayloadClient();
+
+  const footer = await payload.findGlobal({
+    slug: 'footer'
+  });
+
+  const metadata = {
+    title: 'Content not found | r3pwn',
+  } as OpenGraphTags;
+
+  return {
+    props: {
+      metadata,
+      footer
+    },
+    revalidate: SECONDS_PER_DAY * 30
+  }
+}
