6d62b8066a8de755aadbf96f37c0e836fc022f50
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-05-20 01:32:36 -0400

    [feat] add my image to "About Me" page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head';
 
-import { Container, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import AppFooter from '../components/AppFooter';
 import AppHeader from '../components/AppHeader';
 import { getPayloadClient } from "../payload/payloadClient";
 import { SECONDS_PER_DAY } from '../utils/constants';
 import { generateMetadataTags } from '../utils/opengraph-tags';
 import { serializeRichText } from "../utils/payload-richtext";
-import { AboutMeData, FooterData } from '../utils/payload-types';
+import { AboutMeData, FooterData, PayloadMedia } from '../utils/payload-types';
 import { OpenGraphTags, RichTextNode, SocialLink } from "../utils/types";
 
 type Props = {
@@ -17,6 +17,8 @@ type Props = {
 }
 
 export default function About({ data, metadata, footer }: Props) {
+  const profileImage = data.image as PayloadMedia;
+  
   return (
     <>
       <Head>
@@ -26,7 +28,22 @@ export default function About({ data, metadata, footer }: Props) {
       <main>
         <Container maxWidth="lg">
           <Typography variant="h1" gutterBottom sx={{ mt: '1rem' }}>About me</Typography>
-          {serializeRichText(data.content as RichTextNode[])}
+          <Box component="div" sx={{ display: { xs: 'flex', md: 'block' }, flexDirection: 'column'}}>
+            <Box
+              component="img"
+              sx={{
+                borderRadius: '50%',
+                maxWidth: '250px',
+                float: 'left',
+                marginLeft: { xs: 'auto', md: '0' },
+                marginRight: { xs: 'auto', md: '3rem' },
+                marginBottom: { xs: '0', md: '2rem' }
+              }}
+              alt={profileImage.altText}
+              src={profileImage.url}
+            />
+            {serializeRichText(data.content as RichTextNode[])}
+          </Box>
         </Container>
       </main>
       <AppFooter icons={footer.socialLinks as SocialLink[]} text={footer.copyrightText || ''}/>
