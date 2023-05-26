8eed46a82fb33d7319782c140fd21c5903d09d03
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-05-25 20:06:26 -0400

    [site] dynamically generate sitemap as json, convert to xml

--- pages/api/generated-sitemap.ts
+++ pages/api/generated-sitemap.ts
@@ -1,69 +1,66 @@
+import fs from 'fs';
+import { glob } from 'glob';
+import { toXML } from 'jstoxml';
 import { NextApiRequest, NextApiResponse } from "next";
-import getPayloadClient from "../../payload/payloadClient";
-import { BlogPost, Hackathon } from "../../utils/payload-types";
+import path from 'path';
 
-function createBasicEntry(url: string) {
-  return `
-  <url>
-    <loc>${process.env.SITE_HOST}${url}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  `.trimStart();
-}
+const hiddenPages = ['500', '404'];
+const xmlOptions = {
+  header: true,
+  indent: '  '
+};
 
-function createBlogEntry(post: BlogPost) {
-  return `
-  <url>
-    <loc>${process.env.SITE_HOST}/blog/${post.slug}</loc>
-    <lastmod>${post.updatedAt}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  `.trimStart();
-}
+async function getAllPages () {
+  const dir = path.resolve('./.next/server/pages');
 
-function createHackathonEntry(post: Hackathon) {
-  return `
-  <url>
-    <loc>${process.env.SITE_HOST}/hackathons/${post.slug}</loc>
-    <lastmod>${post.updatedAt}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.7</priority>
-  </url>
-  `.trimStart();
+  return await glob(`${dir}/**/*.html`).then(data => {
+    return data.map(file => {
+      let page = file.replace(dir, '');
+      // replace "index.html" with "/"
+      if (page === '/index.html') {
+        page = '/';
+      }
+      return {
+        path: page.replace(/.html$/, ''),
+        stats: fs.statSync(file)
+      }
+    }).filter(page => {
+      return !hiddenPages.includes(page.path.replace(/^\//, ''));
+    });
+  });
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const payload = await getPayloadClient();
+    const pages = await getAllPages();
 
-    const blogPosts = (await payload.find({
-      collection: 'blog-post'
-    })).docs
-  
-    const hackathons = (await payload.find({
-      collection: 'hackathon'
-    })).docs;
-    
-    // generate sitemap here
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${createBasicEntry('')}
-    ${createBasicEntry('/about')}
-    ${createBasicEntry('/blog')}
-    ${blogPosts.map(createBlogEntry).join('')}
-    ${createBasicEntry('/hackathons')}
-    ${hackathons.map(createHackathonEntry).join('')}
-    </urlset>`;
+    // generate sitemap
+    const sitemap = toXML(
+      {
+        _name: 'urlset',
+        _attrs: {
+            xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9'
+        },
+        _content: [
+          pages.map(page => ({
+            url: [
+              { loc: `${process.env.SITE_HOST}${page.path}` },
+              { lastmod: new Date(page.stats.mtime).toISOString() },
+              { changefreq: 'weekly' },
+              { priority: 0.7 }
+            ]
+          }))
+        ]
+      },
+      xmlOptions
+    );
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/xml');
     // Instructing the Vercel edge to cache the file for 1 day
     res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=86400');
 
-    res.end(xml)
+    res.end(sitemap)
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
