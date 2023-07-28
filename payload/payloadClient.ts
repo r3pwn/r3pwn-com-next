678a90fdb56a2d717768119694259ef7aa7d4728
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-07-28 01:39:22 -0400

    payload: add fallbacks in client

--- payload/payloadClient.ts
+++ payload/payloadClient.ts
@@ -1,25 +1,41 @@
-import { NavigationData, PageData } from "./payload-types";
+import { NavigationData, PageData } from './payload-types';
 
 export const getNavigation = async () => {
-  return fetch(`${process.env.SITE_HOST}/api/globals/navigation`)
-    .then(res => res.json())
-    .then(res => res as NavigationData);
+  try {
+    return fetch(`${process.env.SITE_HOST}/api/globals/navigation`)
+      .then(res => res.json())
+      .then(res => res as NavigationData);
+  } catch {
+    return {};
+  }
 }
 
 export const getAllPages = async (draftMode = false) => {
-  return fetch(`${process.env.SITE_HOST}/api/page${!draftMode && '?where[_status][equals]=published'}`)
-  .then(res => res.json())
-  .then(res => res?.docs as PageData[]);
+  try {
+    return fetch(`${process.env.SITE_HOST}/api/page${!draftMode && '?where[_status][equals]=published'}`)
+      .then(res => res.json())
+      .then(res => res?.docs as PageData[]);
+  } catch {
+    return [];
+  }
 }
 
 export const getPagesBySlug = async (pageSlug: string, draftMode = false) => {
-  return fetch(`${process.env.SITE_HOST}/api/page/by-slug/${pageSlug}?draft=${draftMode}`)
-  .then(res => res.json())
-  .then(res => res?.docs as PageData[]);
+  try {
+    return fetch(`${process.env.SITE_HOST}/api/page/by-slug/${pageSlug}?draft=${draftMode}`)
+      .then(res => res.json())
+      .then(res => res?.docs as PageData[]);
+  } catch {
+    return [];
+  }
 }
 
 export const getPagesByParentId = async (parentId: string) => {
-  return fetch(`${process.env.SITE_HOST}/api/page/by-parent/${parentId}`)
-  .then(res => res.json())
-  .then(res => res?.docs as PageData[]);
+  try {
+    return fetch(`${process.env.SITE_HOST}/api/page/by-parent/${parentId}`)
+      .then(res => res.json())
+      .then(res => res?.docs as PageData[]);
+  } catch {
+    return [];
+  }
 }
