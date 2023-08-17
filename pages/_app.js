ba51f478773fc146896c4287243162dc46cc104c
Greg Willard (r3pwn) <wickett06@gmail.com>
2023-08-17 18:59:40 -0400

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+
 import '@/styles/globals.css';
 import '@fontsource/inter/300.css';
 import '@fontsource/inter/400.css';
@@ -8,5 +10,10 @@ import { ThemeProvider } from '@mui/material/styles';
 import { getCurrentTheme } from '../utils/theme';
 
 export default function App({ Component, pageProps }) {
-  return <ThemeProvider theme={getCurrentTheme()}><Component {...pageProps} /></ThemeProvider>;
+  return (
+    <ThemeProvider theme={getCurrentTheme()}>
+      <Component {...pageProps} />
+      <Analytics />
+    </ThemeProvider>
+  );
 }
